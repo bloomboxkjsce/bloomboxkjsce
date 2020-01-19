2930d762876c69caa2358664d0249025be4b2abf
vedangparasnis <35874709+vedangparasnis@users.noreply.github.com>
2020-01-19 12:53:11 +0530

    Configured test on All Route in Development

--- app.test.js
+++ app.test.js
@@ -1,5 +1,10 @@
 const assert = require("assert");
+const url = require("url");
+// host testing
+const mocha = require("mocha");
 
 describe("app", () => {
-  console.log("app is started");
+  it("app", () => {
+    console.log("app is started without any bug");
+  });
 });
